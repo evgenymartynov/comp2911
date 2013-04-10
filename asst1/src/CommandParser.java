d223273005a41a3abac5e5f8837396e34b93272a
Evgeny Martynov <evgeny@epochfail.com>
2013-04-10 16:32:42 +1000

    Throw on very bad input

--- asst1/src/CommandParser.java
+++ asst1/src/CommandParser.java
@@ -5,6 +5,7 @@
  * Author: Evgeny Martynov
  */
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -37,89 +38,97 @@ public class CommandParser {
 		while (commandStream.hasNext()) {
 			String command = commandStream.next();
 
-			// We can probably do this with a more compact method, but is it
-			// really worth the hassle?
-			if (command.equals("Room")) {
-				// Room <capacity> <room>
+			try {
+				// We can probably do this with a more compact method, but is it
+				// really worth the hassle?
+				if (command.equals("Room")) {
+					// Room <capacity> <room>
 
-				int capacity = commandStream.nextInt();
-				String roomName = commandStream.next();
+					int capacity = commandStream.nextInt();
+					String roomName = commandStream.next();
 
-				system.createRoom(roomName, capacity);
-			} else if (command.equals("Book")) {
-				// Book <user> <capacity> <numWeeks> <month> <date> <time>
-				// <duration> <title>
+					system.createRoom(roomName, capacity);
+				} else if (command.equals("Book")) {
+					// Book <user> <capacity> <numWeeks> <month> <date> <time>
+					// <duration> <title>
 
-				String user = commandStream.next();
-				int capacity = commandStream.nextInt();
-				int numWeeks = commandStream.nextInt();
-				String month = commandStream.next();
-				int date = commandStream.nextInt();
-				int time = commandStream.nextInt();
-				int duration = commandStream.nextInt();
-				String title = commandStream.next();
+					String user = commandStream.next();
+					int capacity = commandStream.nextInt();
+					int numWeeks = commandStream.nextInt();
+					String month = commandStream.next();
+					int date = commandStream.nextInt();
+					int time = commandStream.nextInt();
+					int duration = commandStream.nextInt();
+					String title = commandStream.next();
 
-				String response = system.addNewBooking(user, title,
-						new BookingTimePeriod(month, date, time, duration,
-								numWeeks), capacity);
-				if (response == null)
-					response = "Booking rejected";
-				else
-					response = "Room " + response + " assigned";
-				System.out.println(response);
-			} else if (command.equals("Change")) {
-				// Change <user> <room> <numWeeks> <month1> <date1> <time1>
-				// <capacity> <month2> <date2> <time2> <duration2> <title>
-				String user = commandStream.next();
-				String roomName = commandStream.next();
-				int numWeeks = commandStream.nextInt();
-				String oldMonth = commandStream.next();
-				int oldDate = commandStream.nextInt();
-				int oldTime = commandStream.nextInt();
-				int newCapacity = commandStream.nextInt();
-				String newMonth = commandStream.next();
-				int newDate = commandStream.nextInt();
-				int newTime = commandStream.nextInt();
-				int newDuration = commandStream.nextInt();
-				String newTitle = commandStream.next();
+					String response = system.addNewBooking(user, title,
+							new BookingTimePeriod(month, date, time, duration,
+									numWeeks), capacity);
+					if (response == null)
+						response = "Booking rejected";
+					else
+						response = "Room " + response + " assigned";
+					System.out.println(response);
+				} else if (command.equals("Change")) {
+					// Change <user> <room> <numWeeks> <month1> <date1> <time1>
+					// <capacity> <month2> <date2> <time2> <duration2> <title>
+					String user = commandStream.next();
+					String roomName = commandStream.next();
+					int numWeeks = commandStream.nextInt();
+					String oldMonth = commandStream.next();
+					int oldDate = commandStream.nextInt();
+					int oldTime = commandStream.nextInt();
+					int newCapacity = commandStream.nextInt();
+					String newMonth = commandStream.next();
+					int newDate = commandStream.nextInt();
+					int newTime = commandStream.nextInt();
+					int newDuration = commandStream.nextInt();
+					String newTitle = commandStream.next();
 
-				// Ignore old duration -- cancel entire booking.
-				BookingTimePeriod oldPeriod = new BookingTimePeriod(oldMonth,
-						oldDate, oldTime, 0, numWeeks);
-				BookingTimePeriod newPeriod = new BookingTimePeriod(newMonth,
-						newDate, newTime, newDuration, numWeeks);
+					// Ignore old duration -- cancel entire booking.
+					BookingTimePeriod oldPeriod = new BookingTimePeriod(
+							oldMonth, oldDate, oldTime, 0, numWeeks);
+					BookingTimePeriod newPeriod = new BookingTimePeriod(
+							newMonth, newDate, newTime, newDuration, numWeeks);
 
-				String response = system.updateExistingBookings(user, roomName,
-						newTitle, oldPeriod, newPeriod, newCapacity);
-				if (response == null)
-					response = "Booking rejected";
-				else
-					response = "Room " + response + " assigned";
+					String response = system.updateExistingBookings(user,
+							roomName, newTitle, oldPeriod, newPeriod,
+							newCapacity);
+					if (response == null)
+						response = "Booking rejected";
+					else
+						response = "Room " + response + " assigned";
 
-				System.out.println(response);
-			} else if (command.equals("Delete")) {
-				// Delete <user> <room> <numWeeks> <month> <date> <time>
+					System.out.println(response);
+				} else if (command.equals("Delete")) {
+					// Delete <user> <room> <numWeeks> <month> <date> <time>
 
-				String user = commandStream.next();
-				String roomName = commandStream.next();
-				int numWeeks = commandStream.nextInt();
-				String month = commandStream.next();
-				int date = commandStream.nextInt();
-				int time = commandStream.nextInt();
+					String user = commandStream.next();
+					String roomName = commandStream.next();
+					int numWeeks = commandStream.nextInt();
+					String month = commandStream.next();
+					int date = commandStream.nextInt();
+					int time = commandStream.nextInt();
 
-				boolean success = system.deleteBookings(user, roomName,
-						new BookingTimePeriod(month, date, time, 0, numWeeks));
-				System.out.println(success ? "Reservations deleted"
-						: "Deletion rejected");
-			} else if (command.equals("Print")) {
-				// Print <room>
-				String roomName = commandStream.next();
+					boolean success = system.deleteBookings(user, roomName,
+							new BookingTimePeriod(month, date, time, 0,
+									numWeeks));
+					System.out.println(success ? "Reservations deleted"
+							: "Deletion rejected");
+				} else if (command.equals("Print")) {
+					// Print <room>
+					String roomName = commandStream.next();
 
-				String bookings = system.describeBookings(roomName);
-				System.out.println(bookings);
-			} else {
-				throw new IllegalArgumentException("Unexpected command '"
-						+ command + "'");
+					String bookings = system.describeBookings(roomName);
+					System.out.println(bookings);
+				} else {
+					throw new IllegalArgumentException("Unexpected command '"
+							+ command + "'");
+				}
+			} catch (InputMismatchException e) {
+				e.printStackTrace();
+				throw new IllegalArgumentException(
+						"Error in parsing arguments to " + command);
 			}
 		}
 	}
