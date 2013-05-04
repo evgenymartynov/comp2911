f93f973a5827447c56fe89fc9325ab7285eeb7a1
Evgeny Martynov <evgeny@epochfail.com>
2013-05-04 13:53:39 +1000

    Add comments to InputParser

--- asst2/src/InputParser.java
+++ asst2/src/InputParser.java
@@ -9,11 +9,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * This class converts between the file-format description of jobs and our
+ * internal job representation. It strictly enforces the format specified in
+ * assignment spec.
+ */
 public class InputParser {
+    /**
+     * @param input
+     *            Input stream to read and parse.
+     */
     public InputParser(Scanner input) {
         this.inputStream = input;
     }
 
+    /**
+     * Read all of the input stream and return parsed data. May throw an
+     * exception if the input is malformed.
+     *
+     * @return List of jobs, converted into our internal format.
+     */
     public List<Job> parseJobs() {
         List<Job> jobs = new ArrayList<Job>();
 
@@ -34,7 +49,7 @@ public class InputParser {
 
     /**
      * Ensures that next token in the input stream is exactly what we expect it
-     * to be. Will throw an exception on mismatch or read error.
+     * to be and consumes it. Will throw an exception on mismatch or read error.
      *
      * @param token
      *            Expected token, which input must match exactly.
@@ -47,5 +62,8 @@ public class InputParser {
         }
     }
 
+    /**
+     * Input stream of commands.
+     */
     private Scanner inputStream;
 }
