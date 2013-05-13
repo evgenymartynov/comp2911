dc08a3cbeb98aeb868268db87e9635a749872d8a
Evgeny Martynov <evgeny@epochfail.com>
2013-05-13 20:25:57 +1000

    Add lab10 base code

---
+++ lab10/SelectableShape.java
@@ -0,0 +1,24 @@
+import java.awt.Graphics2D;
+
+/**
+ * A shape that manages its selection state.
+ */
+public abstract class SelectableShape implements SceneShape {
+    public void setSelected(boolean b) {
+        selected = b;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void drawSelection(Graphics2D g2) {
+        translate(1, 1);
+        draw(g2);
+        translate(1, 1);
+        draw(g2);
+        translate(-2, -2);
+    }
+
+    private boolean selected;
+}
